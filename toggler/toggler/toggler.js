44afdf2210d938dcfdefb6fcc5e4c1ae67004f0f
Iramis Valentin <21691862+OmnesPotens@users.noreply.github.com>
2020-12-17 16:25:46 -0500

    initial attempt at enabling token visionn+light updates on item equip and unequip

--- toggler/toggler/toggler.js
+++ toggler/toggler/toggler.js
@@ -1,35 +1,51 @@
 const Toggler = (() => {
-  // VERSION INFORMATION
-  const Toggler_Author = "Sky#9453";
-  const Toggler_Version = "0.0.1";
-  const Toggler_LastUpdated = 1605926745;
+	// VERSION INFORMATION
+	const Toggler_Author = "Sky#9453";
+	const Toggler_Version = "0.0.1";
+	const Toggler_LastUpdated = 1605926745;
 
-	function toggleDarkvision(app, html, data) {
+	function toggleVisionEffect(app, html, data) {
 		let items = data.actor.items;
-		let goggles = items.find(i => i.name.toLowerCase() === "goggles of night");
-		if (goggles) {
-			let actor = game.actors.get(data.actor._id);
-			let tokens = actor.getActiveTokens();
-			for (let token of tokens) {
-				if (!token.data.flags?.toggler?.init) {
-					token.update({
-						"flags.toggler.init": true,
-						"flags.toggler.orig_dimSight": token.data.dimSight
-					});
-				}
-				let orig_dimSight = token.data.flags?.toggler?.orig_dimSight;
-				if (goggles.data.equipped) {
-					token.update({ dimSight: orig_dimSight + 60 });
-				} else {
-					token.update({ dimSight: orig_dimSight });
+		// let vision_item = items.find(i => i.transferredEffects().toLowerCase().includes("vision"));
+		Object.entries(items).forEach((item) => {
+			if (item.transferredEffects().toLowerCase().includes("vision")) {
+				let actor = game.actors.get(data.actor._id);
+				let tokens = actor.getActiveTokens();
+				for (let token of tokens) {
+					if (!token.data.flags?.toggler?.init) {
+						let init_flag = "flags.toggler.".concat(token.name.toLowerCase(), ".init");
+						let vision_flag = "flags.toggler.".concat(token.name.toLowerCase(), ".orig_vision");
+						let json_obj = {};
+						json_obj[init_flag] = true;
+						json_obj[vision_flag] = {
+							hasVision: true,
+							dimSight: token.data.dimSight,
+							brightSight: token.data.brightSight,
+							sightAngle: token.data.sightAngle,
+							dimLight: token.datadimLight,
+							brightLight: token.data.brightLight,
+							lightAngle: token.data.lightAngle,
+							lightAnimation: token.data.lightAnimation
+						};
+						console.log("THIS IS MY JSON OBJ")
+						console.log(json_obj)
+						console.log("END OF JSON OBJ")
+						token.update(json_obj);
+					}
+					let orig_vision = token.data.flags?.toggler?.orig_vision;
+					if (item.data.equipped) {
+						token.update({ dimSight: orig_vision.dimSight + 60 });
+					} else {
+						token.update(orig_vision);
+					}
 				}
 			}
-		}
+		});
 	}
-	
-	Hooks.on("ready", function() {
+
+	Hooks.on("ready", function () {
 		Hooks.on("renderActorSheet5eCharacter", (app, html, data) => {
-			toggleDarkvision(app, html, data);
+			toggleVisionEffect(app, html, data);
 		});
 		console.log("-=> Toggler v" + Toggler_Version + " <=- [" + (new Date(Toggler_LastUpdated * 1000)) + "]");
 	});
